296e22b5c25679c38766cc39c753f652ef00b020
sarin-donaa <sarin@donaa.ai>
2023-12-21 22:43:51 +0000

    Update data_extraction.py and Database_utils.py: Ensure that the compability between the files

--- data_extraction.py
+++ data_extraction.py
@@ -1,19 +1,36 @@
 # %%
-import database_utils
+
+
+# %%
+#import database_utils as dbu
 from sqlalchemy import text
+import pandas as pd
 
 class DataExtractor:
-
   def __init__(self, db_connector):
     self.db_connector = db_connector
   
-  def read_data(self, table_name): 
+  def fetch_data(self, table_name): 
     try:
-      with self.db_connector.execution_options(isolation_level='AUTOCOMMIT').connect() as connection: 
+      with self.db_connector.engine.execution_options(isolation_level='AUTOCOMMIT').connect() as connection: 
         query = text(f"SELECT * FROM {table_name}")
         result = connection.execute(query)
         data = result.fetchall()
         return data
     except Exception as e: 
-      print("Error: Data extraction failed\n", e)
+      print("Error: Fetching data failed\n", e)
+    
+  def read_rds_table(self, table_name): 
+    try: 
+      with self.db_connector.engine.execution_options(isolation_level='AUTOCOMMIT').connect() as connection: 
+        rds_pd = pd.read_sql_table('table_name', self.db_connector.engine)
+        rds_pd.head(10)
+        return rds_pd
+    except Exception as e: 
+      print("Error: Reading RDS table failed\n", e)
+# %%
+  check what is causing the error for the table to not load
+  test in the database_tulis directly 
+  if it runs there 
+  export to the data_extraction 
 # %%
--- database_utils.py
+++ database_utils.py
@@ -3,41 +3,40 @@ import yaml
 from sqlalchemy import create_engine, inspect
 
 class DatabaseConnector:
-  creds_yaml = 'db_creds.yaml'
+  def __init__(self, creds_yaml = 'db_creds.yaml'):
+    self.creds_yaml = creds_yaml
+    self.creds_data = self.read_db_creds()
+    self.engine = self.init_db_engine()
 
-  @classmethod
-  def read_db_creds(cls):
+  def read_db_creds(self):
     try: 
-      with open(cls.creds_yaml, 'r') as file:
+      with open(self.creds_yaml, 'r') as file:
         creds_data = yaml.safe_load(file) # Dealing with untrusted input file
+        print("INFO: Credentials Read")
       return creds_data
     except FileNotFoundError: 
       print("ERROR: File not found\n", e)
     except Exception as e: 
       print("ERROR: Unexpected error\n", e)
 
-  @classmethod
-  def init_db_engine(cls):
-    creds_data = cls.read_db_creds()
+  def init_db_engine(self):
     DATABASE_TYPE = 'postgresql'
     DBAPI = 'psycopg2'
-    HOST = creds_data['RDS_HOST']
-    USER = creds_data['RDS_USER']
-    PASSWORD = creds_data['RDS_PASSWORD']
-    DATABASE = creds_data['RDS_DATABASE']
-    PORT = creds_data['RDS_PORT']
+    HOST = self.creds_data['RDS_HOST']
+    USER = self.creds_data['RDS_USER']
+    PASSWORD = self.creds_data['RDS_PASSWORD']
+    DATABASE = self.creds_data['RDS_DATABASE']
+    PORT = self.creds_data['RDS_PORT']
     try: 
       engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-      print("Database engine initilised in DatabaseConnector")
+      print("INFO: Database engine initilised in DatabaseConnector")
       return engine
     except Exception as e: 
       print("ERROR: Database engine initilisation failed\n", e)
     
-  @classmethod
-  def list_db_table(cls): 
-    engine = cls.init_db_engine()
+  def list_db_tables(self): 
     try: 
-      inspector = inspect(engine)
+      inspector = inspect(self.engine)
       table_name = inspector.get_table_names()
       print("Table in database: ", table_name)
       return table_name
