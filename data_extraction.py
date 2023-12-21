5331a4efed7e8ab17b5fb212b0fe2785cb0a97c4
sarin-donaa <sarin@donaa.ai>
2023-12-21 15:12:28 +0000

    Update data_extraction.py, Added read_data

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,19 @@
 # %%
 import database_utils
-
+from sqlalchemy import text
 
 class DataExtractor:
 
+  def __init__(self, db_connector):
+    self.db_connector = db_connector
   
+  def read_data(self, table_name): 
+    try:
+      with self.db_connector.execution_options(isolation_level='AUTOCOMMIT').connect() as connection: 
+        query = text(f"SELECT * FROM {table_name}")
+        result = connection.execute(query)
+        data = result.fetchall()
+        return data
+    except Exception as e: 
+      print("Error: Data extraction failed\n", e)
+# %%
