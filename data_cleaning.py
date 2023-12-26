d35ae684cefcdc34063c0eca0c2015bd38a2fa8b
sarin-donaa <sarin@donaa.ai>
2023-12-26 19:48:58 +0000

    Update data_cleaning.py, .gitignore

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,28 +7,35 @@ class DataCleaning:
 
   def clean_card_data(self, pdf_df):
     #15587 = 55 + 56*277 + 20 <- (19 data + 1 heading)
-    
+  
+    df = pdf_df.copy()
+   
+
+    ## card_number errors
+
     # Card Number range between 16 - 19 digits
     # int64   9,223,372,036,854,775,807
     # uint64 18,446,744,073,709,551,615
     # int64 does not cover all 19 digit variations 
 
-    ## card_number errors
-    pdf_df["card_number"] = pd.to_numeric(pdf_df["card_number"], errors='coerce', downcast='unsigned')
-    pdf_df.dropna(axis=0, inplace=True)
+    df['card_number'] = pd.to_numeric(df['card_number'], errors='coerce', downcast='unsigned')
+    df.dropna(axis=0, inplace=True)
     try: 
-      pdf_df["card_number"] = pdf_df["card_number"].astype('uint64')
+      df['card_number'] = df['card_number'].astype('uint64')
     except Exception as e: 
       print("ERROR: Failed to clean card number coloumn as unsigned integer\n", e)
     
     #expiry_date   
-    pdf_df["expiry_date"] = pd.to_datetime(pdf_df["expiry_date"], format='%m/%d', errors='coerce', exact=True)
-    pdf_df.dropna(axis=0, inplace=True)
-    pdf_df["expiry_date"] = pdf_df["expiry_date"].dt.strftime('%m/%d')
+    df['expiry_date'] = pd.to_datetime(df['expiry_date'], format='%m/%d', errors='coerce', exact=True)
+    df.dropna(axis=0, inplace=True)
+    df['expiry_date'] = df['expiry_date'].dt.strftime('%m/%d')
 
     #card_provider 
-    #date_payment_confirmed
+    df['card_provider'] = df['card_provider'].astype('string')
 
-    return pdf_df_clean
+    #date_payment_confirmed
+    df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], format='%Y-%m-%d', errors='coerce', exact=True)
+    df.dropna(axis=0, inplace=True)
+    return df
 
 
---
+++ main.py
@@ -0,0 +1,6 @@
+#%%
+from data_extraction import DataExtractor
+from database_utils import DatabaseConnector
+from data_cleaning import DataCleaning
+
+# %%
