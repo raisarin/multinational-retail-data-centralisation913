371a36ae3f1ad30c94b283c720f634935e72fcce
sarin-donaa <sarin@donaa.ai>
2023-12-28 02:46:15 +0000

    Updated data_cleaning.py with clean_user_data method

--- data_extraction.py
+++ data_extraction.py
@@ -7,22 +7,21 @@ class DataExtractor:
   def __init__(self, db_connector):
     self.db_connector = db_connector
   
-  def fetch_data(self, table_name): 
-    try:
-      with self.db_connector.engine.execution_options(isolation_level='AUTOCOMMIT').connect() as connection: 
-        query = text(f"SELECT * FROM {table_name}")
-        result = connection.execute(query)
-        data = result.fetchall()
-        return data
-    except Exception as e: 
-      print("Error: Fetching data failed\n", e)
+  # def fetch_data(self, table_name): 
+  #   try:
+  #     with self.db_connector.engine.execution_options(isolation_level='AUTOCOMMIT').connect() as connection: 
+  #       query = text(f"SELECT * FROM {table_name}")
+  #       result = connection.execute(query)
+  #       data = result.fetchall()
+  #       return data
+  #   except Exception as e: 
+  #     print("Error: Fetching data failed\n", e)
     
   def read_rds_table(self, table_name): 
     try: 
-      with self.db_connector.engine.execution_options(isolation_level='AUTOCDEOMMIT').connect() as connection: 
-        table_df = pd.read_sql_table(table_name, self.db_connector.engine)
-        print(F"INFO: Dataframe read from table {table_name}")
-        return table_df
+      table_df = pd.read_sql_table(table_name, self.db_connector.engine)
+      print(F"INFO: Dataframe read from table {table_name}")
+      return table_df
     except Exception as e: 
       print("Error: Reading RDS table failed\n", e)
   
