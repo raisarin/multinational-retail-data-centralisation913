b0315dd7b00ca8e32f2fc7d172a5e04c0e690563
sarin-donaa <sarin@donaa.ai>
2024-01-05 16:59:49 +0000

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,27 +3,34 @@ from dateutil.parser import parse
 
 class DataCleaning: 
   
+  def clean_country_code(self, df):
+    df['country_code'] = df['country_code'].str.replace('GGB', 'GB')
+    df = df[df['country_code'].str.len() == 2]
+    return df
+  
+  #phone number 
+  def clean_date(self, df, coloumn_name):
+    df.loc[:,coloumn_name] = df[coloumn_name].apply(parse)
+    df[coloumn_name] = pd.to_datetime(df[coloumn_name], errors='coerce')
+    df.dropna(axis=0, inplace=True)
+    df[coloumn_name] = df[coloumn_name].dt.strftime('%Y-%m-%d')
+    return df
+  
+  def clean_phone_number(self, df):
+    df['phone_number'] = df['phone_number'].replace({r'\+49':'0', r'\+44':'0', r'\(0\)':''}, regex=True)
+    df['phone_number'] = df['phone_number'].replace({'D': ''}, regex=True)
+    return df
+    
   def clean_user_data(self, df):
     df.drop_duplicates()
     df = df[~df.isin(['NULL']).any(axis=1)]
-
-    def clean_country_code(self, df):
-      df['country_code'] = df['country_code'].str.replace('GGB', 'GB')
-      df = df[df['country_code'].str.len() == 2]
-
-    #phone number 
-    def clean_date(self, df, coloumn_name):
-      df.loc[:,coloumn_name] = df[coloumn_name].apply(parse)
-      df[coloumn_name] = pd.to_datetime(df[coloumn_name], errors='coerce')
-      df.dropna(axis=0, inplace=True)
-      df[coloumn_name] = df[coloumn_name].dt.strftime('%Y-%m-%d')
-    
-    def clean_phone_number(self, df):
-      df['phone_number'] = df['phone_number'].replace({r'\+49':'0', r'\+44':'0', r'\(0\)':''}, regex=True)
-      df['phone_number'] = df['phone_number'].replace({'\D': ''}, regex=True)
-    
+    df = self.clean_country_code(df)
+    df = self.clean_date(df, 'date_of_birth')
+    df = self.clean_date(df, 'join_date')
+    df = self.clean_phone_number(df)
     return df
   
+
   def clean_card_data(self, df):
     #15587 = 55 + 56*277 + 20 <- (19 data + 1 heading)
     ## card_number errors
