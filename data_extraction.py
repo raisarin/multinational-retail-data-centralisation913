382885ffe2e26d72e75e243ac7b73b1443c1fc79
raisarin <raisarin@yahoo.com>
2024-03-23 16:15:47 +0000

    Update database_extraction.py: Add classmethod list_db_table

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,16 @@
+# %%
+import database_utils
+from sqlalchemy import inspect
+
 class DataExtractor:
-  def
+
+  @classmethod
+  def list_db_table(cls): 
+    engine = database_utils.DatabaseConnector.init_db_engine()
+    print("Database engine initilised from DataExtractor")
+    print(engine)
+    inspector = inspect(engine)
+    table_name = inspector.get_table_names()
+    print("Table name: ", table_name)
+    return table_name
+  
