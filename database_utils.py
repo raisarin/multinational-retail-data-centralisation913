bd4654c522aef2858acef7b59355a3dd2f9fcada
raisarin <raisarin@yahoo.com>
2024-03-23 16:15:47 +0000

    Update data_extraction.py, Added read_data

--- database_utils.py
+++ database_utils.py
@@ -12,9 +12,9 @@ class DatabaseConnector:
         creds_data = yaml.safe_load(file) # Dealing with untrusted input file
       return creds_data
     except FileNotFoundError: 
-      print("ERROR: File not found", e)
+      print("ERROR: File not found\n", e)
     except Exception as e: 
-      print("ERROR: Unexpected error", e)
+      print("ERROR: Unexpected error\n", e)
 
   @classmethod
   def init_db_engine(cls):
@@ -28,17 +28,20 @@ class DatabaseConnector:
     PORT = creds_data['RDS_PORT']
     try: 
       engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-      engine.execution_options(isolation_level='AUTOCOMMIT').connect()
       print("Database engine initilised in DatabaseConnector")
       return engine
     except Exception as e: 
-      print("ERROR: Database engine initilisation failed", e)
+      print("ERROR: Database engine initilisation failed\n", e)
     
   @classmethod
   def list_db_table(cls): 
     engine = cls.init_db_engine()
-    inspector = inspect(engine)
-    table_name = inspector.get_table_names()
-    print("Table in database: ", table_name)
-    return table_name
+    try: 
+      inspector = inspect(engine)
+      table_name = inspector.get_table_names()
+      print("Table in database: ", table_name)
+      return table_name
+    except Exception as e: 
+      print("Error: Engine inspection failed\n", e)
+
 # %%
